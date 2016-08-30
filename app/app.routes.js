ffabae7f3c2ae9bd7f901659454697a12d925503
Arian Rahimi <itsarianr@gmail.com>
2016-08-30 18:12:11 +0430

    Changed nested states and nested views architure for ui-router.

--- app/app.routes.js
+++ app/app.routes.js
@@ -1,27 +1,37 @@
 'use strict';
 
-app.config(['$locationProvider', function($locationProvider) {
-    $locationProvider.html5Mode(true);
+app.config(['$locationProvider', function ($locationProvider) {
+  $locationProvider.html5Mode(true);
 }]);
 
-app.config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/messenger/');
-    $stateProvider
-        .state('messenger', {
-            url: '/messenger/:chatId',
-            views: {
-                '': {
-                    templateUrl: 'app/components/messenger/messengerView.html'
-                },
-                'header@messenger': {
-                    templateUrl: 'app/components/messenger/header/headerView.html'
-                },
-                'channels@messenger': {
-                    templateUrl: 'app/components/messenger/channels/channelsView.html'
-                },
-                'messages@messenger': {
-                    templateUrl: 'app/components/messenger/messages/messagesView.html'
-                }
-            }
-        });
+app.config(function ($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/messenger/');
+  $stateProvider
+    .state('messenger', {
+      abstract: true,
+      url: '/messenger',
+      views: {
+        '': {
+          templateUrl: 'app/components/messenger/messengerView.html'
+        },
+        'channels@messenger': {
+          templateUrl: 'app/components/messenger/channels/channelsView.html'
+        }
+      }
+    })
+    .state('messenger.home', {
+      url: '/',
+      template: '<h1>لطفا یک گروه را انتخاب کنید.</h1>'
+    })
+    .state('messenger.messages', {
+      url: '/:chatId',
+      views: {
+        '': {
+          templateUrl: 'app/components/messenger/messages/messagesView.html'
+        },
+        'header@messenger.messages': {
+          templateUrl: 'app/components/messenger/header/headerView.html'
+        }
+      }
+    });
 });
