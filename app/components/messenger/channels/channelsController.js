91e708faa39e48b7f59894a5dc5c9ba3107280a7
Mohsen Iranmanesh <apple@Apples-MacBook-Air.local>
2016-09-28 20:51:45 +0330

    Creating new channel window implemented

--- app/components/messenger/channels/channelsController.js
+++ app/components/messenger/channels/channelsController.js
@@ -10,6 +10,11 @@ app.controller('channelsController', function ($scope, $stateParams, channelsSer
     DIRECT: 2
   };
 
+  var clearNewChannelForm = function(){
+    $scope.newChannel.name = "";
+    $scope.newChannel.description = "";
+    $scope.newChannel.isPrivate = false;
+  }
   $scope.filterByPublicAndPrivate = function (channel) {
     return channel.type === channelType.PUBLIC || channel.type === channelType.PRIVATE;
   };
@@ -18,9 +23,22 @@ app.controller('channelsController', function ($scope, $stateParams, channelsSer
     return channel.type === channelType.DIRECT;
   };
 
+  $scope.createChannelInitial = function () {
+  }
 
+  $scope.createChannelSubmit = function () {
+    clearNewChannelForm();
+  }
+
+  $scope.exitCreateChannel = function(){
+    clearNewChannelForm();
+  }
   channelsService.getChannels().then(function (data) {
     $scope.channels = data;
   });
+
+  $scope.checkTextSizeValidation = function(text, size){
+    console.log(text);
+  }
 
 });
