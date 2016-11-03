36811229d7a2d35901e20a007a7ff3f5412914f0
Arian Rahimi <itsarianr@gmail.com>
2016-11-03 10:56:54 +0330

    Fixed authentication request.

--- app/shared/auth/authService.js
+++ app/shared/auth/authService.js
@@ -5,9 +5,15 @@ app.factory('authService', ['$http', '$localStorage', function ($http,
 
   return {
     login: function (username, password) {
-      $http.post('/api/v1/auth/login/', {
+      var data = {
         username: username,
         password: password
+      };
+      $http({
+        method: 'POST',
+        url: '/api/v1/auth/login/',
+        data: data,
+        skipAuthorization: true
       }).then(function (response) {
         if (response.data.token) {
           $localStorage.userToken = response.data.token;
