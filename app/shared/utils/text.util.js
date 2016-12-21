7090bdd119dc31903817d2b68e1e0235aa64328b
Arian Rahimi <itsarianr@gmail.com>
2016-12-21 21:53:14 +0330

    Added convert to persian number process.

--- app/shared/utils/text.util.js
+++ app/shared/utils/text.util.js
@@ -9,13 +9,36 @@ app.factory('textUtil', function () {
 
   function urlify(text) {
     var urlRegex = /(https?:\/\/[^\s]+)/g;
-    return text.replace(urlRegex, function(url) {
-        return '<a href="' + url + '" target="_blank">' + url + '</a>';
+    return text.replace(urlRegex, function (url) {
+      return '<a href="' + url + '" target="_blank">' + url + '</a>';
     });
+  }
+
+  function persianify(text) {
+    var persian = {
+      0: '۰',
+      1: '۱',
+      2: '۲',
+      3: '۳',
+      4: '۴',
+      5: '۵',
+      6: '۶',
+      7: '۷',
+      8: '۸',
+      9: '۹'
+    };
+    var list = text.match(/[0-9]/g);
+    if (list !== null && list.length !== 0) {
+      for (var i = 0; i < list.length; i++) {
+        text = text.replace(list[i], persian[list[i]]);
+      }
+    }
+    return text;
   }
 
   return {
     isEnglish: isEnglish,
-    urlify: urlify
+    urlify: urlify,
+    persianify: persianify
   };
 });
