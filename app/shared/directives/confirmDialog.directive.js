ffc786983e3b27df14fe0912972afb7562afd017
AmirHossein Ameli <amirahosseinameli@gmail.com>
2017-05-01 10:09:51 +0430

    Fixed and Added ConfirmDialog

--- app/shared/directives/confirmDialog.directive.js
+++ app/shared/directives/confirmDialog.directive.js
@@ -1,19 +1,38 @@
 'use strict';
 
-app.directive('confirmDialog', function () {
+app.directive('confirmDialog', function() {
+
+  function getTemplate(element, attrs) {
+    var templateString = '<button class="{{class}}" ' +
+      'mwl-confirm message="{{message}}" confirm-text="{{ok}}" animation="true" cancel-text="{{cancel}}" placement="bottom" on-confirm="callUpdate()" confirm-button-type="{{confirmButton}}" cancel-button-type="default">';
+    if (attrs.icon)
+      templateString += '<span class="{{icon}}"></span>';
+    else
+      templateString += '{{buttonName}}';
+    templateString += '</button>';
+    return templateString;
+  }
   return {
     scope: {
-      buttonName:'@',
-      message:'@',
-      cancel:'@',
-      ok:'@',
-      confirmFunc:'&',
+      class: '@',
+      icon: '@',
+      buttonName: '@',
+      message: '@',
+      cancel: '@',
+      ok: '@',
+      confirmButton: '@',
+      confirmFunc: '&',
+      args: '=',
     },
-    template: '<button class="danger-btn" mwl-confirm message="{{message}}" confirm-text="{{ok}}" cancel-text="{{cancel}}" placement="bottom" on-confirm="callUpdate()" confirm-button-type="danger" cancel-button-type="default">{{buttonName}}</button>',
     replace: true,
-    link: function(scope, elm, attrs){
+    template: getTemplate,
+    link: function(scope, elm, attrs) {
       scope.callUpdate = function() {
-        scope.confirmFunc()();
+        var fn = scope.confirmFunc();
+        if (!scope.args)
+          fn();
+        else
+          fn.apply(null, scope.args);
       };
     }
   };
