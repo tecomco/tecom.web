d3619d1763fc23f072f0ac6025bb135000f96a63
AmirHossein Ameli <amirahosseinameli@gmail.com>
2017-05-02 17:29:44 +0430

    Break long line into shorter lines

--- app/shared/directives/confirmDialog.directive.js
+++ app/shared/directives/confirmDialog.directive.js
@@ -4,7 +4,10 @@ app.directive('confirmDialog', function() {
 
   function getTemplate(element, attrs) {
     var templateString = '<button class="{{class}}" ' +
-      'mwl-confirm message="{{message}}" confirm-text="{{ok}}" animation="true" cancel-text="{{cancel}}" placement="bottom" on-confirm="callUpdate()" confirm-button-type="{{confirmButton}}" cancel-button-type="default">';
+      'mwl-confirm message="{{message}}" confirm-text="{{ok}}" ' +
+      ' animation="true" cancel-text="{{cancel}}" placement="bottom" ' +
+      ' on-confirm="callUpdate()" confirm-button-type="{{confirmButton}}" ' +
+      ' cancel-button-type="default">';
     if (attrs.icon)
       templateString += '<span class="{{icon}}"></span>';
     else
