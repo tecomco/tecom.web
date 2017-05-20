1df02b140e2974e9034cabd315742a783119d9e9
Arian Rahimi <itsarianr@gmail.com>
2017-05-20 12:15:50 +0430

    Updated versioning for modified resources.

--- app/components/login/login.module.js
+++ app/components/login/login.module.js
@@ -14,7 +14,7 @@ var app = angular.module('LoginApp', [
           url: '/login',
           views: {
             '': {
-              templateUrl: 'app/components/login/login-form.html?v=1.0.1'
+              templateUrl: 'app/components/login/login-form.html?v=1.0.2'
             }
           },
           onEnter: function ($window) {
