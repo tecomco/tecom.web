3e1e7fe62a4210051dd0e4e2c39f73c15dc7a7a2
kiarashws <kiarashny@gmail.com>
2017-08-13 13:45:53 +0430

    Colors provided.

--- app/shared/auth/user.model.js
+++ app/shared/auth/user.model.js
@@ -1,14 +1,36 @@
 'use strict';
 
-app.factory('User', function () {
+app.factory('User', function() {
 
   function User(id, username, email, image) {
     this.id = id;
     this.username = username;
     this.email = email;
     this.image = image || '/static/img/user-def.png';
-    this.usernameColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+    this.usernameColor = User.COLORS[Math.floor(Math.random() * User.COLORS.length)];
   }
+
+  User.COLORS = [
+    '#e53935',
+    '#d81b60',
+    '#8e24aa',
+    '#5e35b1',
+    '#3949ab',
+    '#1e88e5',
+    '#039be5',
+    '#00acc1',
+    '#00897b',
+    '#43a047',
+    '#7cb342',
+    '#afb42b',
+    '#f57f17',
+    '#ff8f00',
+    '#ef6c00',
+    '#f4511e',
+    '#6d4c41',
+    '#757575',
+    '#546e7a'
+  ];
 
   return User;
 });
