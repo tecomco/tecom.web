59f5261b1b16c04eea4945cc92a7fb8dd10a570b
AmirHossein Ameli <amirahosseinameli@gmail.com>
2017-10-11 19:30:32 +0330

    implemented squirrel handling in main.js

--- installer.js
+++ installer.js
@@ -5,14 +5,14 @@
 const electronInstaller = require('electron-winstaller');
 
 const resultPromise = electronInstaller.createWindowsInstaller({
-    appDirectory: "C:/Users/AmirHossein Ameli/Desktop/dev/dev/tecom.web/Tecom-win32-x64",
-    outputDirectory: 'C:/Users/AmirHossein Ameli/Desktop/dev/dev/tecom.web/Tecom-win32-x64/release',
-    exe: 'Tecom.exe',
-    loadingGif: 'C:/Users/AmirHossein Ameli/Desktop/dev/dev/tecom.web/static/img/loading.gif',
-    iconUrl: 'C:/Users/AmirHossein Ameli/Desktop/dev/dev/tecom.web/favicon.ico',
-    setupIcon : 'C:/Users/AmirHossein Ameli/Desktop/dev/dev/tecom.web/favicon.ico',
-    setupExe : 'Tecom.exe',
-    noMsi : 'No',
-    noDelta : 'Yes'
-  });
-  resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
+  appDirectory: "C:/Users/AmirHossein Ameli/Desktop/Tecom-clone/tecom.web/Tecom-win32-x64",
+  outputDirectory: 'C:/Users/AmirHossein Ameli/Desktop/Tecom-clone/tecom.web/Tecom-win32-x64/release',
+  exe: 'Tecom.exe',
+  loadingGif: 'C:/Users/AmirHossein Ameli/Desktop/Tecom-clone/tecom.web/static/img/loading.gif',
+  iconUrl: 'C:/Users/AmirHossein Ameli/Desktop/Tecom-clone/tecom.web/favicon.ico',
+  setupIcon: 'C:/Users/AmirHossein Ameli/Desktop/Tecom-clone/tecom.web/favicon.ico',
+  setupExe: 'Tecom.exe',
+  noMsi: 'No'
+});
+resultPromise.then(() => console.log("It worked!"),
+  (e) => console.log(`No dice: ${e.message}`));
